60392bd66fb201c9d4c439147bb5ac168dca391f
SUMIT SHARMA <sumit140803@gmail.com>
2025-07-29 13:33:40 +0530

    day-18(3)

--- day_18/src/components/Form.jsx
+++ day_18/src/components/Form.jsx
@@ -1,24 +1,36 @@
 import React, { useState } from 'react'
 
 const Form = () => {
-    const [name , setname] = useState("sumit")
-    const [email , setemail] = useState("sumit@gmail.com")
-    const [number , setnumber] = useState(1234567890)
+  const [Formdata, setFormdata] =useState({
+    name:"",
+    email:"",
+    number:""
+  })
+    // const [name , setname] = useState("sumit")
+    // const [email , setemail] = useState("sumit@gmail.com")
+    // const [number , setnumber] = useState(1234567890)
+    const handlechange =(e)=>{
+      const {value , name} = e.target
+      setFormdata({
+        ...Formdata,
+        [name]:value,
+      })
+    }
 
     const submithandler = (e) =>{
         e.preventDefault()
-        console.log(name)
-        console.log(email)
-        console.log(number)
+        console.log(Formdata.name)
+        console.log(Formdata.email)
+        console.log(Formdata.number)
 
     }
   return (
-    <div>
-        <form onSubmit={submithandler} action="">
-            <input onChange={(e)=>setname(e.target.value)} value={name} type="text" placeholder='name' />
-            <input onChange={(e)=>setemail(e.target.value)} value={email} type="email" name="email" placeholder='email'/>
-            <input onChange={(e)=>setnumber(e.target.value)} value={number} type="number" name="number" placeholder='75666876' />
-            <button className='bg-red-400'>OK</button>
+    <div className=' bg-gray-100 h-screen w-screen flex justify-center items-center flex-col gap-5'>
+        <form className='flex justify-center items-center flex-col gap-5 bg-gray-500 p-10 rounded-2xl ' onSubmit={submithandler} action="">
+            <input className='bg-gray-300 w-[400px] px-5 py-2 border-none rounded' value={Formdata.name} onChange={handlechange} type="text" placeholder='name' name='name' />
+            <input  className='bg-gray-300 w-[400px] px-5 py-2 border-none rounded' value={Formdata.email} onChange={handlechange} type="email" name="email" placeholder='email'/>
+            <input  className='bg-gray-300 w-[400px]  px-5 py-2 border-none rounded' value={Formdata.number} onChange={handlechange} type="number" name="number" placeholder='75666876' />
+            <button className='bg-blue-400 w-[400px] text-white px-8 py-2 rounded-[10px] border-0 font-medium'>OK</button>
         </form>
     </div>
   )
