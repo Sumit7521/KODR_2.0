8dbc8669a636694b7c4ccb42e91fe13f2981ba4e
SUMIT SHARMA <sumit140803@gmail.com>
2025-07-29 12:11:22 +0530

    day-18

---
+++ day_18/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  let greeting ="hello"
+  const [count , setcount]= useState(0)
+
+  const increment =()=>{
+    setcount(count+1)
+  }
+  const decrement =()=>{
+    setcount(count-1)
+  }
+  return (
+    <div className='flex flex-col items-center justify-center bg-gray-600 h-screen w-screen'>
+      <h1 className='text-3xl'>{count}</h1>
+      <div className='flex gap-3'>
+        <button onClick={increment} className='border-2 h-5 w-10 flex items-center justify-center'>+</button>
+        <button onClick={decrement} className='border-2 h-5 w-10 flex items-center justify-center'>-</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
